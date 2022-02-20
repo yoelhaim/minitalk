2f3724353ac6f2f936942e1956a524437aaf9464
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2022-02-20 16:57:56 +0100

    fixed : fix error make

--- utils.h
+++ utils.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 17:06:27 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/02/19 17:19:19 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/02/20 15:40:13 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
+
+struct s_utils
+{
+	int	i;
+	int	pid;
+}t_utils;
 
 void	ft_putnbr(unsigned int nb);
 int		ft_atoi(const char *str);
