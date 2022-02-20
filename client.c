00c8b31d48331e91b989f6ed0180043acd530d35
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2022-02-20 16:23:14 +0100

    add struct in mandatory

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 17:24:41 by yoelhaim          #+#    #+#             */
-/*   Updated: 2022/02/19 17:20:06 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2022/02/20 15:31:39 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,22 +29,19 @@ void	sending(char c, int pid)
 
 int	main(int ac, char **av)
 {
-	int					pid;
-	int					i;
-
-	i = 0;
+	t_utils.i = 0;
 	if (ac != 3)
 	{
 		write (1, "Usage: ./client [server PID] [string to send]\n", 47);
 		exit(1);
 	}
-	pid = ft_atoi(av[1]);
-	if (pid < 1)
+	t_utils.pid = ft_atoi(av[1]);
+	if (t_utils.pid < 1)
 	{
 		write(1, "Bad pid \n", 10);
 		exit(1);
 	}
-	while (av[2][i])
-		sending(av[2][i++], pid);
+	while (av[2][t_utils.i])
+		sending(av[2][t_utils.i++], t_utils.pid);
 	return (0);
 }
